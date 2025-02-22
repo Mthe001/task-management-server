e6747aa31f86e199b20e4356e01f37933310a76e
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-02-22 18:40:43 +0600

    feat:for deploy

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
 
@@ -266,12 +266,6 @@ async function run() {
        
 
 
-
-     
-
-
-
-
         // Route to get tasks by email
         app.get("/tasks/:email", async (req, res) => {
             try {
@@ -324,10 +318,6 @@ async function run() {
        
 
 
-
-
-
-
         app.get("/tasks", async (req, res) => {
             try {
                 const tasks = await tasksCollection.find().toArray();
@@ -344,15 +334,6 @@ async function run() {
 
 
 
-
-
-
-
-
-
-
-
-
            
 
         app.get('/', (req, res) => {
@@ -365,18 +346,14 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
-
 
 
 
